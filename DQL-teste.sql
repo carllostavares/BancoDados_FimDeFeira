352f9bb436f19b5654747e177291852a10938403
Carlos Tavares <carlostavares2350@gmail.com>
2023-05-22 15:46:17 -0300

    Adicionando arquivo DQL teste

---
+++ DQL-teste.sql
@@ -0,0 +1,12 @@
+insert into tb_cliente values ("11122233332","carlos","carlos@.com.br","M");
+INSERT INTO tb_produto values (1,50.00,"feijão",20,250,"97.776.3530001");
+INSERT INTO tb_parceiro values ("97.776.3530001","Mercado de Juca","juca.123@meq.com.br");
+INSERT INTO tb_itens_pedido values (1,1,100,20.00);
+select * from tb_cliente;
+select * from tb_parceiro;
+select * from tb_produto;
+select * from  tb_itens_pedido;
+
+
+ -- now()
+
